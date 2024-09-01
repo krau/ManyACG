6e7dc09c4d0063d4365973233591e496efa748d5
krau <71133316+krau@users.noreply.github.com>
2024-09-01 10:25:01 +0800

    refactor: remove unused code in common/version.go

--- common/version.go
+++ common/version.go
@@ -1,17 +1,7 @@
 package common
 
-import (
-	"time"
-)
-
 var (
 	Version   string = "dev"
 	BuildTime string = "Unknown"
 	Commit    string = "Unknown"
 )
-
-func init() {
-	if BuildTime == "Unknown" {
-		BuildTime = time.Now().Format("2006-01-02 15:04:05")
-	}
-}
