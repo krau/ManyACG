558ddde7a26e9e11b70c0085f8f64bcc65ab6886
krau <71133316+krau@users.noreply.github.com>
2024-11-16 15:12:38 +0800

    feat: fetch artwork by url in inline query

--- telegram/handlers/inline_query.go
+++ telegram/handlers/inline_query.go
@@ -8,6 +8,7 @@ import (
 	"github.com/krau/ManyACG/adapter"
 	"github.com/krau/ManyACG/common"
 	"github.com/krau/ManyACG/service"
+	"github.com/krau/ManyACG/sources"
 	"github.com/krau/ManyACG/telegram/utils"
 	"github.com/krau/ManyACG/types"
 
@@ -20,6 +21,34 @@ import (
 
 func InlineQuery(ctx context.Context, bot *telego.Bot, query telego.InlineQuery) {
 	queryText := query.Query
+	if url := sources.FindSourceURL(queryText); url != "" {
+		artwork, err := service.GetArtworkByURLWithCacheFetch(ctx, url)
+		if err != nil {
+			bot.AnswerInlineQuery(telegoutil.InlineQuery(query.ID, telegoutil.ResultArticle(uuid.NewString(), "获取作品失败", telegoutil.TextMessage(url))))
+			return
+		}
+		results := make([]telego.InlineQueryResult, 0, min(len(artwork.Pictures), 48))
+		caption := utils.GetArtworkHTMLCaption(artwork)
+		for _, picture := range artwork.Pictures {
+			if picture.TelegramInfo != nil && picture.TelegramInfo.PhotoFileID != "" {
+				result := telegoutil.ResultCachedPhoto(uuid.NewString(), picture.TelegramInfo.PhotoFileID).WithCaption(caption).WithParseMode(telego.ModeHTML)
+				results = append(results, result)
+				continue
+			}
+			result := telegoutil.ResultPhoto(uuid.NewString(),
+				fmt.Sprintf("https://wsrv.nl/?url=%s&w=2560&h=2560&we",
+					picture.Original), picture.Thumbnail).WithCaption(caption).WithParseMode(telego.ModeHTML)
+			results = append(results, result)
+		}
+		if err := bot.AnswerInlineQuery(&telego.AnswerInlineQueryParams{
+			InlineQueryID: query.ID,
+			Results:       results,
+			CacheTime:     10,
+		}); err != nil {
+			Logger.Errorf("响应Inline查询失败: %s", err)
+		}
+		return
+	}
 	texts := common.ParseStringTo2DArray(queryText, "|", " ")
 	artworks, err := service.QueryArtworksByTexts(ctx, texts, types.R18TypeAll, 48, adapter.OnlyLoadPicture())
 	if err != nil || len(artworks) == 0 {
@@ -43,6 +72,6 @@ func InlineQuery(ctx context.Context, bot *telego.Bot, query telego.InlineQuery)
 		Results:       results,
 		CacheTime:     1,
 	}); err != nil {
-		Logger.Errorf("回复查询失败: %s", err)
+		Logger.Errorf("响应Inline查询失败: %s", err)
 	}
 }
