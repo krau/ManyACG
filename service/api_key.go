2bf8f86573dc2cc29fa2df9193f3b36383975fa9
krau <71133316+krau@users.noreply.github.com>
2025-09-29 22:02:52 +0800

    feat: impl api key crud

--- service/api_key.go
+++ service/api_key.go
@@ -3,37 +3,95 @@ package service
 import (
 	"context"
 
-	"github.com/krau/ManyACG/dao"
+	"github.com/krau/ManyACG/internal/infra/database"
+	"github.com/krau/ManyACG/internal/infra/database/model"
 	"github.com/krau/ManyACG/types"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func CreateApiKey(ctx context.Context, key string, quota int, permissions []types.ApiKeyPermission, description string) (*types.ApiKeyModel, error) {
-	apiKey := &types.ApiKeyModel{
-		Key:         key,
-		Quota:       quota,
-		Used:        0,
-		Permissions: permissions,
+	// apiKey := &types.ApiKeyModel{
+	// 	Key:         key,
+	// 	Quota:       quota,
+	// 	Used:        0,
+	// 	Permissions: permissions,
+	// 	Description: description,
+	// }
+	// _, err := dao.CreateApiKey(ctx, apiKey)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// return dao.GetApiKeyByKey(ctx, key)
+	apiKey := &model.ApiKey{
+		Key:   key,
+		Quota: quota,
+		Used:  0,
+		Permissions: func() []string {
+			var perms []string
+			for _, p := range permissions {
+				perms = append(perms, string(p))
+			}
+			return perms
+		}(),
 		Description: description,
 	}
-	_, err := dao.CreateApiKey(ctx, apiKey)
+	_, err := database.Default().CreateApiKey(ctx, apiKey)
 	if err != nil {
 		return nil, err
 	}
-	return dao.GetApiKeyByKey(ctx, key)
+	id, err := database.Default().GetApiKeyByKey(ctx, key)
+	if err != nil {
+		return nil, err
+	}
+	return &types.ApiKeyModel{
+		ID:    primitive.ObjectID(id.ID.ToObjectID()),
+		Key:   id.Key,
+		Quota: id.Quota,
+		Used:  id.Used,
+		Permissions: func() []types.ApiKeyPermission {
+			var perms []types.ApiKeyPermission
+			for _, p := range id.Permissions {
+				perms = append(perms, types.ApiKeyPermission(p))
+			}
+			return perms
+		}(),
+		Description: id.Description,
+	}, nil
 }
 
 func GetApiKeyByKey(ctx context.Context, key string) (*types.ApiKeyModel, error) {
-	return dao.GetApiKeyByKey(ctx, key)
+	// return dao.GetApiKeyByKey(ctx, key)
+	apiKey, err := database.Default().GetApiKeyByKey(ctx, key)
+	if err != nil {
+		return nil, err
+	}
+	return &types.ApiKeyModel{
+		ID:    primitive.ObjectID(apiKey.ID.ToObjectID()),
+		Key:   apiKey.Key,
+		Quota: apiKey.Quota,
+		Used:  apiKey.Used,
+		Permissions: func() []types.ApiKeyPermission {
+			var perms []types.ApiKeyPermission
+			for _, p := range apiKey.Permissions {
+				perms = append(perms, types.ApiKeyPermission(p))
+			}
+			return perms
+		}(),
+		Description: apiKey.Description,
+	}, nil
 }
 
 func IncreaseApiKeyUsed(ctx context.Context, key string) error {
-	return dao.IncreaseApiKeyUsed(ctx, key)
+	// return dao.IncreaseApiKeyUsed(ctx, key)
+	return database.Default().IncreaseApiKeyUsed(ctx, key)
 }
 
 func AddApiKeyQuota(ctx context.Context, key string, quota int) error {
-	return dao.AddApiKeyQuota(ctx, key, quota)
+	// return dao.AddApiKeyQuota(ctx, key, quota)
+	return database.Default().AddApiKeyQuota(ctx, key, quota)
 }
 
 func DeleteApiKey(ctx context.Context, key string) error {
-	return dao.DeleteApiKey(ctx, key)
+	// return dao.DeleteApiKey(ctx, key)
+	return database.Default().DeleteApiKey(ctx, key)
 }
