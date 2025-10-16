3cabf6f767ac5582b18dafb55c7dff6fa279e739
krau <71133316+krau@users.noreply.github.com>
2025-10-16 19:34:15 +0800

    chore: style code

--- internal/infra/config/runtimecfg/common.go
+++ internal/infra/config/runtimecfg/common.go
@@ -1,6 +1,5 @@
 package runtimecfg
 
-
 type CookieConfig struct {
 	Name  string `toml:"name" mapstructure:"name" json:"name" yaml:"name"`
 	Value string `toml:"value" mapstructure:"value" json:"value" yaml:"value"`
