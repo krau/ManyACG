01f5fba63fbcaa319efa95318aeb795ccdeab8bc
krau <71133316+krau@users.noreply.github.com>
2025-12-10 20:31:56 +0800

    feat: add PGroonga index optimizations and reindexing support

--- internal/infra/config/runtimecfg/database.go
+++ internal/infra/config/runtimecfg/database.go
@@ -1,6 +1,13 @@
 package runtimecfg
 
 type databaseConfig struct {
-	Type         string `toml:"type" mapstructure:"type" json:"type" yaml:"type"`
-	DSN          string `toml:"dsn" mapstructure:"dsn" json:"dsn" yaml:"dsn"`
+	Type  string      `toml:"type" mapstructure:"type" json:"type" yaml:"type"`
+	DSN   string      `toml:"dsn" mapstructure:"dsn" json:"dsn" yaml:"dsn"`
+	Pgsql pgsqlConfig `toml:"pgsql" mapstructure:"pgsql" json:"pgsql" yaml:"pgsql"`
+}
+
+type pgsqlConfig struct {
+	PGroonga        bool `toml:"pgroonga" mapstructure:"pgroonga" json:"pgroonga" yaml:"pgroonga"`
+	// PGroonga index isn't crash safe. You need to run REINDEX when your PGroonga index is broken by crash.
+	ReindexPGroonga bool `toml:"reindex_pgroonga" mapstructure:"reindex_pgroonga" json:"reindex_pgroonga" yaml:"reindex_pgroonga"`
 }
--- internal/infra/database/db.go
+++ internal/infra/database/db.go
@@ -108,11 +108,68 @@ func optimizePgSQL(ctx context.Context, db *gorm.DB) error {
 	if db.Dialector.Name() != "postgres" {
 		return nil
 	}
+	pgroongaEnabled := runtimecfg.Get().Database.Pgsql.PGroonga
+	if pgroongaEnabled {
+		// use pgroonga indexes
+		log.Debug("Applying pgroonga index optimizations...")
+		sqls := []string{
+			`CREATE EXTENSION IF NOT EXISTS pgroonga;`,
+
+			// delete existing trigram indexes if any
+			`DROP INDEX IF EXISTS idx_artworks_title_trgm;`,
+			`DROP INDEX IF EXISTS idx_artworks_description_trgm;`,
+			`DROP INDEX IF EXISTS idx_artists_name_trgm;`,
+			`DROP INDEX IF EXISTS idx_tags_name_trgm;`,
+			`DROP INDEX IF EXISTS idx_tag_alias_alias_trgm;`,
+			// PGroonga indexes
+			`CREATE INDEX IF NOT EXISTS pgroonga_artworks_title ON artworks USING pgroonga (title);`,
+			`CREATE INDEX IF NOT EXISTS pgroonga_artworks_description ON artworks USING pgroonga (description);`,
+			`CREATE INDEX IF NOT EXISTS pgroonga_artists_name ON artists USING pgroonga (name);`,
+			`CREATE INDEX IF NOT EXISTS pgroonga_tags_name ON tags USING pgroonga (name);`,
+			`CREATE INDEX IF NOT EXISTS pgroonga_tag_alias_alias ON tag_aliases USING pgroonga (alias);`,
+
+			// Foreign key
+			`CREATE INDEX IF NOT EXISTS idx_artworks_artist_id ON artworks(artist_id);`,
+			`CREATE INDEX IF NOT EXISTS idx_artwork_tags_artwork_id ON artwork_tags(artwork_id);`,
+			`CREATE INDEX IF NOT EXISTS idx_artwork_tags_tag_id ON artwork_tags(tag_id);`,
+		}
+		for _, sql := range sqls {
+			if err := db.WithContext(ctx).Exec(sql).Error; err != nil {
+				return err
+			}
+		}
+		log.Debug("pgroonga index optimizations applied")
+		if runtimecfg.Get().Database.Pgsql.ReindexPGroonga {
+			log.Debug("Reindexing pgroonga indexes...")
+			pgroongaIndexes := []string{
+				"pgroonga_artworks_title",
+				"pgroonga_artworks_description",
+				"pgroonga_artists_name",
+				"pgroonga_tags_name",
+				"pgroonga_tag_alias_alias",
+			}
+			for _, index := range pgroongaIndexes {
+				reindexSQL := "REINDEX INDEX " + index + ";"
+				if err := db.WithContext(ctx).Exec(reindexSQL).Error; err != nil {
+					return err
+				}
+			}
+			log.Debug("Reindexing of pgroonga indexes completed")
+		}
+		return nil
+	}
+	// by default, use pg_trgm indexes
 	log.Debug("Applying pg_trgm index optimizations...")
 
 	sqls := []string{
 		// Extensions
 		`CREATE EXTENSION IF NOT EXISTS pg_trgm;`,
+		// delete existing pgroonga indexes if any
+		`DROP INDEX IF EXISTS pgroonga_artworks_title;`,
+		`DROP INDEX IF EXISTS pgroonga_artworks_description;`,
+		`DROP INDEX IF EXISTS pgroonga_artists_name;`,
+		`DROP INDEX IF EXISTS pgroonga_tags_name;`,
+		`DROP INDEX IF EXISTS pgroonga_tag_alias_alias;`,
 
 		// Trigram indexes
 		`CREATE INDEX IF NOT EXISTS idx_artworks_title_trgm ON artworks USING gin (title gin_trgm_ops);`,
